521fdaf42f021a1ed4308690afcea46044b2435e
WangLu <cs3cx4g@gmail.com>
2022-07-30 13:18:44 +0000

    update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,7 +1,7 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme";
 import { searchPlugin } from "@vuepress/plugin-search";
-const { googleAnalyticsPlugin } = require("@vuepress/plugin-google-analytics");
+import { googleAnalyticsPlugin } from "@vuepress/plugin-google-analytics";
 export default defineUserConfig({
   lang: "zh-CN",
   title: "Mereith's Gists",
